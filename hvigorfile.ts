103436315fda6a5e939fea396b7b0d9a9a479461
ZXHHYJ <957447668@qq.com>
2024-11-19 21:37:41 +0800

    移除了一些无用文件。

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,7 +1,6 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
-import { stormTypeSafePlugin } from '@zxhhyj/storm-typesafe-plugin'
 
 export default {
     system: appTasks,
-    plugins: [stormTypeSafePlugin()]
+    plugins: []
 }
--- plugin/Index.ts
+++
@@ -1,1 +0,0 @@
-export { stormTypeSafePlugin } from "./src/Main";
